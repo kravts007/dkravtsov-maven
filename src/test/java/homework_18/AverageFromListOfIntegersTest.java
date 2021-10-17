de084d4017555732ebd2723bce4984f3c9824f31
Dmytro Kravtsov <kravts0@gmail.com>
2021-10-17 10:01:29 +0300

    HomeWork 18 Collections of Integers and String sorted by streams

--- src/test/java/homework_18/AverageFromListOfIntegersTest.java
+++ src/test/java/homework_18/AverageFromListOfIntegersTest.java
@@ -5,27 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 
 public class AverageFromListOfIntegersTest {
     static List<Integer> integers = new ArrayList<>();
 
     @Test
     public void calculateAverageColTest() {
-        Random rand = new Random();
-        rand.setSeed(System.currentTimeMillis());
-        for (int i = 0; i < 10; i++) {
-            Integer r = rand.nextInt(256);
-            integers.add(r);
+        List<Integer> collection = new ArrayList<>();
+        int colSize = 5;
+        //expected
+        double expectedAverage = 0.0;
+        for (int i = 0; i < colSize; i++) {
+            collection.add(i);
+            expectedAverage += i;
         }
-        Assertions.assertEquals(
-                //expected
-                integers.stream()
-                        .mapToDouble(Integer::doubleValue)
-                        .sum()
-                        /integers.size(),
-                //actual
-                AverageFromListOfIntegers.calculateAverageCol(integers),
-                0.01);
+        expectedAverage /= colSize;
+        //actual
+        double actualAverage = AverageFromListOfIntegers.calculateAverageCol(collection);
+        Assertions.assertEquals(expectedAverage, actualAverage);
     }
 }
