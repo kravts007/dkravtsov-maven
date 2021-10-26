3baecad2901faff76efe6bc9e3b0695a55683726
Dmytro Kravtsov <kravts0@gmail.com>
2021-10-26 13:19:05 +0300

    HomeWork 21 ATM program with Threads

--- src/main/java/homework_21/ATM.java
+++ src/main/java/homework_21/ATM.java
@@ -2,16 +2,20 @@ package homework_21;
 
 import java.util.Scanner;
 
-public class ATM {
+public class ATM extends Thread {
     private final int withdrawMaxValue;
     private final int putInMaxValue;
     private int sum;
+    final Object lock = new Object();
 
     public ATM(int sum, int puttingMaxValue, int withdrawMaxValue) {
         this.withdrawMaxValue = withdrawMaxValue;
         this.putInMaxValue = puttingMaxValue;
         this.sum = sum;
     }
+
+    Thread tr2 = new Thread("Putter");
+    Thread tr1 = new Thread("Taker");
 
     /**
      * randomCash() - randomizer for withdrawing and depositing limit of money
@@ -26,32 +30,33 @@ public class ATM {
      */
     public void getCash() {
         int cashTakeOff = randomCash(withdrawMaxValue);
-        if (sum >= cashTakeOff) {
-            sum -= cashTakeOff;
-            System.out.println(Thread.currentThread().getName() + " - withdraw: " + cashTakeOff + " - Money in ATM: " + sum);
-        } else {
-            Scanner sc = new Scanner(System.in);
-            while (sum < cashTakeOff) {
-                System.out.println(Thread.currentThread().getName() + " - want to withdraw: " + cashTakeOff);
-                System.out.println("Not enough money at the ATM: " + sum);
-                System.out.println("Enter an amount less than " + sum);
-                cashTakeOff = sc.nextInt();
+        synchronized (lock) {
+            if (sum >= cashTakeOff) {
                 sum -= cashTakeOff;
-                System.out.println("Total money at ATM " + sum);
+                System.out.println(tr1.getName() + " - withdraw: " + cashTakeOff + " - Money in ATM: " + sum);
+            } else {
+                Scanner sc = new Scanner(System.in);
+                while (sum < cashTakeOff) {
+                    System.out.println(tr1.getName() + " - want to withdraw: " + cashTakeOff);
+                    System.out.println("Not enough money at the ATM: " + sum);
+                    System.out.println("Enter an amount less than " + sum);
+                    cashTakeOff = sc.nextInt();
+                    sum -= cashTakeOff;
+                    System.out.println("Total money at ATM " + sum);
+                }
             }
         }
     }
-
     /**
      * putCash() - Put a random amount of money in the ATN
      */
     public void putCash() {
-        int cashPutIn = randomCash(putInMaxValue);
-        this.sum += cashPutIn;
-        System.out.println(Thread.currentThread() + " - put: " + cashPutIn + " - Money in ATM: " + sum);
-
+        synchronized (lock) {
+            int cashPutIn = randomCash(putInMaxValue);
+            this.sum += cashPutIn;
+            System.out.println(tr2.getName() + " - put: " + cashPutIn + " - Money in ATM: " + sum);
+        }
     }
-
     @Override
     public String toString() {
         return "ATM{" +
